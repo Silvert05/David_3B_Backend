0522cbec14af31780e89037b32ae8dbd102a4d71
David <cochadavid2005@gmail.com>
2024-12-08 14:29:30 -0500

    categoria funcional

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,13 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   // swagger
   const config = new DocumentBuilder()
-  .setTitle('cats example')
+  .setTitle('Product example')
   .setDescription('The Produc API description')
   .setVersion('1.0')
   .addTag('product')
   .build();
   const document = SwaggerModule.createDocument(app,config);
   SwaggerModule.setup('api',app,document);
-  await app.listen(3000 , 'localhost');
+  await app.listen(3000 );
 }
 bootstrap();
