697f6c800f7663ede434e3ac2628bc325c0fecca
David <cochadavid2005@gmail.com>
2024-12-09 19:06:53 -0500

    clase 16 terminado

--- src/modules/producto/producto.module.ts
+++ src/modules/producto/producto.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ProductoService } from './producto.service';
 import { ProductoController } from './producto.controller';
+import { Producto } from './entities/producto.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Producto])],
   controllers: [ProductoController],
   providers: [ProductoService],
 })
